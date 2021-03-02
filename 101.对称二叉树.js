b32505dff489c8a67eda5674d60f0e3bf481da47
yangyuting <seeyuyu@163.com>
2021-03-02 23:04:16 +0800

    1

---
+++ 101.对称二叉树.js
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=javascript
+ *
+ * [101] 对称二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isSymmetric = function(root) {
+    function check(left,right){
+        if(left==null && right==null){
+            return true
+        }
+        if(left && right){
+            return left.val == right.val && check(left.left, right.right) && check(left.right, right.left);
+        }
+        return false
+    }
+    if (root == null) {     // 如果传入的root就是null，对称
+        return true;
+    }           
+    return check(root.left, root.right); // 否则，判断它的左右子树是否满足对称
+};
+// @lc code=end
+
